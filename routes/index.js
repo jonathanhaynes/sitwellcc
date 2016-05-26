02cf95bbd2791fc230a2f7f42bda6cb1790655b9
Jonny Haynes <jonny@colouringcode.com>
2016-05-26 08:55:52 +0100

    Sorted routing and middleware so now FB and IG media works, added a sort function so now FB media comes out in the correct order

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,18 @@
 var express = require('express'),
     router = express.Router(),
-    ig = require('instagram-node').instagram(),
-    PowerHouse = require('powerhouse-js'),
-    moment = require('moment'),
-    fb = require('fb');
+    PowerHouse = require('powerhouse-js');
 
-ig.use({
-  client_id: '25f9fc11c5474e69ae6220c1d5095f2d',
-  client_secret: '43b4227a99c545dcab7cc0b7a7eaf566',
-  access_token: '2399073333.25f9fc1.47ed772a4e2940acbfc7e2c54fc129ef'
-});
 
-fb.options({
-  appId: '984371968323534',
-  version: 'v2.5'
-});
 
-router.get('/', function(req, res, next) {
+var igAPI = function(req, res, next) {
+
+  var ig = require('instagram-node').instagram();
+
+  ig.use({
+    client_id: '25f9fc11c5474e69ae6220c1d5095f2d',
+    client_secret: '43b4227a99c545dcab7cc0b7a7eaf566',
+    access_token: '2399073333.25f9fc1.47ed772a4e2940acbfc7e2c54fc129ef'
+  });
 
   var igMedia = [];
 
@@ -29,32 +25,110 @@ router.get('/', function(req, res, next) {
         'caption': media.caption.text
       });
     });
+
+    req.igMedia = igMedia;
+    next();
   });
+};
+
+var fbAPI = function (req, res, next) {
+
+  var fb = require('fb');
+
+  fb.options({
+    appId: '984371968323534',
+    version: 'v2.5'
+  });
+
+  var dateNow = Math.round(new Date().getTime()/1000.0),
+      moment = require('moment'),
+      fbMedia = [];
+
+  fb.api(
+    '/1076165799068349/',
+    'GET',
+    {
+      "access_token" : "CAANZCRZCVZCr84BAM1q36ZBQOvEujS0Jjq1ZBavTXtIpDjZAQyLYwCZBovmrDHWIUmkGzPJsO5sdsM48HZAvBwebAJqIBhsBeEBnG9LxVHUdvaNVwGhQfXC9WHNbBzQ7GjrmjLrjZCuYYHXxmNXpZBCuB1C0T22WOfEqNqOxHn4rb6rmXZAaWaNjs1HH1PEfnpkSIsZD",
+      "fields" : "events.since(" + dateNow + ")"
+    },
+    function(response) {
+
+      var filteredFbMedia = response.events.data.sort(dynamicSort('start_time'));
+
+      filteredFbMedia.forEach(function(item, i){
+        fbMedia.push({
+          'title': item.name,
+          'description': linkify(item.description),
+          'link': 'https://www.facebook.com/events/' + item.id + '/',
+          'location': item.place.name,
+          'date': moment(item.start_time).format("dddd Do MMMM YYYY")
+        });
+      });
+
+      req.fbMedia = fbMedia;
+      next();
+    }
+  );
+
+  function dynamicSort(property) {
+    var sortOrder = 1;
+
+    if(property[0] === "-") {
+      sortOrder = -1;
+      property = property.substr(1);
+    }
+
+    return function (a,b) {
+      var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    };
+  }
+
+  function nth(d) {
+    if(d>3 && d<21) return 'th'; // thanks kennebec
+    switch (d % 10) {
+      case 1:  return "st";
+      case 2:  return "nd";
+      case 3:  return "rd";
+      default: return "th";
+    }
+  }
+
+  function linkify(text) {
+    if( !text ) return text;
+
+    text = text.replace(/((https?\:\/\/|ftp\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,function(url){
+      nice = url;
+      if( url.match('^https?:\/\/') )
+      {
+        nice = nice.replace(/^https?:\/\//i,'');
+      }
+      else
+        url = 'http://'+url;
+
+
+      return '<a target="_blank" href="'+ url +'">'+ nice.replace(/^www./i,'') +'</a>';
+    });
+
+    return text;
+  }
+};
+
+router.get('/', igAPI);
+router.get('/', function(req, res, next) {
 
   res.locals.meta = {
     title: 'Sitwell Cycling Club, Whiston, Rotherham - Founded 2016',
     description: 'Founded January 2016. Rotherham\'s newest cycling club serving Whiston and the surrounding areas. Come and join us for a club ride on a Wednesday evening or Sunday morning. For the good times.'
   };
 
-  res.locals.instagram = igMedia;
-
-  res.render('pages/index');
+  res.render('pages/index', {
+    instagram: req.igMedia
+  });
 });
 
-router.get('/about', function(req, res, next) {
-
-  var igMedia = [];
-
-  ig.tag_media_recent('sitwellcc', {'count': 20}, function(err, medias, pagination, remaining, limit) {
-
-    PowerHouse.forEach(medias, function(media, i) {
-      igMedia.push({
-        'link': media.link,
-        'src': media.images.standard_resolution.url,
-        'caption': media.caption.text
-      });
-    });
-  });  
+router.get('/about', igAPI);
+router.get('/about', function(req, res, next) { 
 
   res.locals.meta = {
     title: 'About - Sitwell Cycling Club', 
@@ -63,9 +137,9 @@ router.get('/about', function(req, res, next) {
     content: 'about'
   };
 
-  res.locals.instagram = igMedia;
-
-  res.render('pages/show');
+  res.render('pages/show', {
+     instagram: req.igMedia
+  });
   
 });
 
@@ -102,6 +176,7 @@ router.get('/about', function(req, res, next) {
     res.render('pages/show');
   });
 
+router.get('/club-rides', igAPI);
 router.get('/club-rides', function(req, res, next) {
   res.locals.meta = {
     title: 'Club Rides - Sitwell Cycling Club',
@@ -110,9 +185,12 @@ router.get('/club-rides', function(req, res, next) {
     content: 'rides' 
   };
 
-  res.render('pages/show');
+  res.render('pages/show', {
+     instagram: req.igMedia
+  });
 });
 
+  router.get('/club-rides/wednesday-evening', igAPI);
   router.get('/club-rides/wednesday-evening', function(req, res, next) {
     res.locals.meta = {
       title: 'Wednesday Evening - Club Rides - Sitwell Cycling Club',
@@ -121,67 +199,14 @@ router.get('/club-rides', function(req, res, next) {
       content: 'wednesday' 
     };
 
-    res.render('pages/show');
+    res.render('pages/show', {
+       instagram: req.igMedia
+    });
   });
 
+  router.get('/club-rides/sunday-morning', fbAPI);
+  router.get('/club-rides/sunday-morning', igAPI);
   router.get('/club-rides/sunday-morning', function(req, res, next) {
-
-    var dateNow = Math.round(new Date().getTime()/1000.0),
-        fbMedia = [];
-
-    fb.api(
-      '/1076165799068349/',
-      'GET',
-      {
-        "access_token" : "CAANZCRZCVZCr84BAM1q36ZBQOvEujS0Jjq1ZBavTXtIpDjZAQyLYwCZBovmrDHWIUmkGzPJsO5sdsM48HZAvBwebAJqIBhsBeEBnG9LxVHUdvaNVwGhQfXC9WHNbBzQ7GjrmjLrjZCuYYHXxmNXpZBCuB1C0T22WOfEqNqOxHn4rb6rmXZAaWaNjs1HH1PEfnpkSIsZD",
-        "fields" : "events.since(" + dateNow + ")"
-      },
-      function(response) {
-
-        response.events.data.forEach(function(item, i){
-          fbMedia.push({
-            'title': item.name,
-            'description': linkify(item.description),
-            'link': 'https://www.facebook.com/events/' + item.id + '/',
-            'location': item.place.name,
-            'date': moment(item.start_time).format("dddd Do MMMM YYYY")
-          });
-        });
-
-        console.log(fbMedia);
-
-        return fbMedia;
-      }
-    );
-
-    function nth(d) {
-      if(d>3 && d<21) return 'th'; // thanks kennebec
-      switch (d % 10) {
-        case 1:  return "st";
-        case 2:  return "nd";
-        case 3:  return "rd";
-        default: return "th";
-      }
-    }
-
-    function linkify(text) {
-      if( !text ) return text;
-
-      text = text.replace(/((https?\:\/\/|ftp\:\/\/)|(www\.))(\S+)(\w{2,4})(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/gi,function(url){
-        nice = url;
-        if( url.match('^https?:\/\/') )
-        {
-          nice = nice.replace(/^https?:\/\//i,'');
-        }
-        else
-          url = 'http://'+url;
-
-
-        return '<a target="_blank" href="'+ url +'">'+ nice.replace(/^www./i,'') +'</a>';
-      });
-
-      return text;
-    }
 
     res.locals.meta = {
       title: 'Sunday Morning - Club Rides - Sitwell Cycling Club',
@@ -190,12 +215,14 @@ router.get('/club-rides', function(req, res, next) {
       content: 'sunday'
     };
 
-    res.locals.facebook = fbMedia;
-
-    res.render('pages/show');
+    res.render('pages/show', {
+      facebook: req.fbMedia,
+      instagram: req.igMedia
+    });
 
   });
 
+router.get('/kit', igAPI);
 router.get('/kit', function(req, res, next) {
   res.locals.meta = {
     title: 'Kit - Sitwell Cycling Club', 
@@ -204,9 +231,12 @@ router.get('/kit', function(req, res, next) {
     content: 'kit' 
   };
 
-  res.render('pages/show');
+  res.render('pages/show', {
+     instagram: req.igMedia
+  });
 });
 
+router.get('/membership', igAPI);
 router.get('/membership', function(req, res, next) {
   res.locals.meta = {
     title: 'Membership - Sitwell Cycling Club', 
@@ -215,7 +245,9 @@ router.get('/membership', function(req, res, next) {
     content: 'membership' 
   };
 
-  res.render('pages/show');
+  res.render('pages/show', {
+     instagram: req.igMedia
+  });
 });
 
 router.get('/news', function(req, res, next) {
