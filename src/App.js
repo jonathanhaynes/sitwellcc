1bfdbf432cb24602e1f4cb1442635ec359f70a5f
Jonny Haynes <jonny@colouringcode.com>
2018-02-14 20:35:47 +0000

    Test page

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ import Help from './Help';
 import NotFound from './NotFound';
 import './App.css';
 
+import Page from './Page';
+
 const App = (props) => (
   <Router>
     <Switch>
       <Redirect exact from="/" to="/help"/>
       <Route exact path="/help" component={Help} />
       <Route exact path="/preview" render={routeProps => <Preview {...routeProps} prismicCtx={props.prismicCtx} />} />
+      <Route exact path="/page/:uid" render={routeProps => <Page {...routeProps} prismicCtx={props.prismicCtx} />} />
       <Route component={NotFound} />
     </Switch>
   </Router>
